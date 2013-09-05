e48cfe05ec631406e5d37513b0532bcced720024
Rossiani Wijaya <rwijaya@moodle.com>
2013-09-05 13:37:29 +0800

    MDL-40829: courses heading: remove 'headerblock' class from header and fix heading levels

--- course/renderer.php
+++ course/renderer.php
@@ -1132,9 +1132,11 @@ class core_course_renderer extends plugin_renderer_base {
         $classes = trim('coursebox clearfix '. $additionalclasses);
         if ($chelper->get_show_courses() >= self::COURSECAT_SHOW_COURSES_EXPANDED) {
             $nametag = 'h3';
+            $nameattributes = array();
         } else {
             $classes .= ' collapsed';
             $nametag = 'div';
+            $nameattributes = array('class' => 'name');
         }
 
         // .coursebox
@@ -1150,7 +1152,7 @@ class core_course_renderer extends plugin_renderer_base {
         $coursename = $chelper->get_course_formatted_name($course);
         $coursenamelink = html_writer::link(new moodle_url('/course/view.php', array('id' => $course->id)),
                 $coursename, array('class' => $course->visible ? '' : 'dimmed'));
-        $content .= html_writer::tag($nametag, $coursenamelink, array('class' => 'name'));
+        $content .= html_writer::tag($nametag, $coursenamelink, $nameattributes);
 
         // If we display course in collapsed form but the course has summary or course contacts, display the link to the info page.
         $content .= html_writer::start_tag('div', array('class' => 'moreinfo'));
@@ -1544,7 +1546,7 @@ class core_course_renderer extends plugin_renderer_base {
                     array('title' => get_string('numberofcourses'), 'class' => 'numberofcourse'));
         }
         $content .= html_writer::start_tag('div', array('class' => 'info'));
-        $content .= html_writer::tag(($depth > 1) ? 'h4' : 'h3', $categoryname, array('class' => 'name'));
+        $content .= html_writer::tag(($depth > 1) ? 'h4' : 'h3', $categoryname);
         $content .= html_writer::end_tag('div'); // .info
 
         // add category content to the output
