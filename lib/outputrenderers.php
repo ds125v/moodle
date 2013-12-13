58d8bb7297e81fb172dee268c4f267ad73121f70
David Scotson <david.scotson@glasgow.ac.uk>
2013-12-13 15:48:25 +0000

    MDL-43362 bootstrap: style fatal error messages

--- lib/outputrenderers.php
+++ lib/outputrenderers.php
@@ -2570,7 +2570,7 @@ EOD;
             $message .= '<p class="errormessage">' . get_string('installproblem', 'error') . '</p>';
             //It is usually not possible to recover from errors triggered during installation, you may need to create a new database or use a different database prefix for new installation.
         }
-        $output .= $this->box($message, 'errorbox', null, array('data-rel' => 'fatalerror'));
+        $output .= $this->box($message, 'errorbox alert alert-error', null, array('data-rel' => 'fatalerror'));
 
         if ($CFG->debugdeveloper) {
             if (!empty($debuginfo)) {
